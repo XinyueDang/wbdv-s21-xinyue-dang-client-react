b3828cfe431f11169724e6b3714f981a37ea4989
xinyueDang <xinyue.dang@algodepth.com>
2021-02-18 11:10:11 -0500

    Update Router part during class

---
+++ src/components/course-manager.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import CourseTable from "./course-table/course-table";
+import CourseGrid from "./course-grid/course-grid";
+import CourseEditor from "./course-editor/course-editor";
+import "./course-manager.css";
+import {Link, Route} from "react-router-dom";
+import courseService from "../services/course-service";
+
+class CourseManager extends React.Component {
+  state = {
+    courses: [],
+    qwe: 123,
+    sdf: 456
+  }
+  updateCourse = (course) => {
+     console.log(course)
+     courseService.updateCourse(course._id, course)
+         .then(status => this.setState((prevState) => ({
+           ...prevState,
+           courses: prevState.courses.map(
+               (c) => c._id === course._id ? course : c)
+         })))
+      }
+  componentDidMount = () =>
+      courseService.findAllCourses()
+          .then(courses => this.setState({courses}))
+
+  addCourse = () => {
+    const newCourse = {
+      title: "New Course",
+      owner: "New Owner",
+      lastModified: "Never"
+    }
+    courseService.createCourse(newCourse)
+            .then(course => this.setState(
+                (prevState) => ({
+                  ...prevState,
+                  courses: [
+                      ...prevState.courses,
+                      course
+                  ]
+                })))
+  }
+
+  deleteCourse = (courseToDelete) => {
+      courseService.deleteCourse(courseToDelete._id)
+          .then(status => {
+            this.setState((prevState) => ({
+                ...prevState,
+                courses: prevState.courses.filter
+                  (course => course !== courseToDelete)
+            }))
+          })
+    }
+
+  render() {
+    return(
+          <div>
+            <Link to="/">
+              <i className="fas fa-2x fa-home float-right"></i>
+            </Link>
+              <h1>Course Manager</h1>
+              <button onClick={this.addCourse}>Add Course</button>
+              <Route path="/courses/table">
+                <CourseTable
+                    updateCourse={this.updateCourse}
+                    deleteCourse={this.deleteCourse}
+                    courses={this.state.courses}/>
+              </Route>
+              <Route path="/courses/grid">
+                <CourseGrid
+                    deleteCourse={this.deleteCourse}
+                    courses={this.state.courses}/>
+              </Route>
+                {/*<Route path="/courses/editor">*/}
+                {/*    <CourseEditor/>*/}
+                {/*</Route>*/}
+                {/*<Route path="/courses/editor"*/}
+                {/*       render={(props) => <CourseEditor props={props}/>}>*/}
+                {/*</Route>*/}
+                <Route path="/courses/editor"
+                       render={(props) => <CourseEditor {...props}/>}>
+                </Route>
+            </div>
+    )
+  }
+}
+
+export default CourseManager
--- src/services/course-service.js
+++ src/services/course-service.js
@@ -1,40 +1,38 @@
-function CourseService() {
-    this.createCourse = createCourse;
-    this.findAllCourses = findAllCourses;
-    this.findCourseById = findCourseById;
-    this.deleteCourse = deleteCourse;
-    this.updateCourse = updateCourse;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001054614/courses';
-    var self = this;
-    function createCourse(course){
-        return fetch(self.url, {
-            method: 'POST',
-            headers: {
-                'content-type':'application/json'
-            },
-            body: JSON.stringify(course)
-        }).then(response => response.json())
-    }
+const COURSES_URL = "https://wbdv-generic-server.herokuapp.com/api/001054614/courses";
 
-    function findAllCourses(){
-        return fetch(self.url)
-            .then(response => response.json())
-    }
-    function findCourseById(id){
-        return fetch(`${self.url}/${id}`)
-            .then(response => response.json())
-    }
-    function updateCourse(id, course){
-        return fetch(`${self.url}/${id}`, {
-            method: 'PUT',
-            headers:{
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(course)
-        }).then(response => response.json())
-    }
-    function deleteCourse(id){
-        return fetch(`${self.url}/${id}`,
-              {method: 'DELETE'})
-    }
+export const findAllCourses = () =>
+    fetch(COURSES_URL)
+        .then(response => response.json())
+
+export const deleteCourse = (courseId) =>
+    fetch(`${COURSES_URL}/${courseId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+
+export const createCourse = (course) =>
+    fetch(COURSES_URL, {
+        method: 'POST',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const updateCourse = (courseId, course) =>
+    fetch(`${COURSES_URL}/${courseId}`, {
+        method: 'PUT',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export default {
+    findAllCourses: findAllCourses,
+    deleteCourse: deleteCourse,
+    createCourse: createCourse,
+    updateCourse: updateCourse
 }
