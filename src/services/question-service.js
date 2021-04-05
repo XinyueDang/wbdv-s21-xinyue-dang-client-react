f2062d042efd5be00797cf53fb5e94c5cece3fee
XinyueDang <sinreydang@gmail.com>
2021-04-05 15:12:32 -0400

    deploy to heroku

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,6 @@
-const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+// const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+const QUIZZES_URL = 'https://wbdv-s21-xinyue-dang-node.herokuapp.com/api/quizzes';
+
 const findQuestionsForQuiz = (qid) => {
    return fetch(`${QUIZZES_URL}/${qid}/questions`)
       .then(response => response.json())
--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,4 +1,6 @@
-const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+// const QUIZZES_URL = 'http://localhost:3001/api/quizzes';
+const QUIZZES_URL = 'https://wbdv-s21-xinyue-dang-node.herokuapp.com/api/quizzes';
+
 const findAllQuizzes = () => {
    return fetch(QUIZZES_URL)
       .then(response => response.json())
